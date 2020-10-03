e2ef0526059295faa39022c6f2db19b8ea42a531
Nivrith <nivrithgomatam@gmail.com>
2020-10-03 12:35:26 +0930

    Add example env config

--- tools/build/app-env.ts
+++ tools/build/app-env.ts
@@ -15,9 +15,24 @@ declare global {
        *
        */
       runtimeChecks: boolean;
+
+      /**
+       * @description
+       * Log exceptions to third party exception tracker (e.g. Sentry, Log Rocket)
+       */
+      logExceptions: boolean;
+
+      /**
+       * @description
+       * Url to post exception logs
+       * 
+       */
+      loggerUrl: string;
     };
   }
 
 export const DEFAULT_APP_ENV: typeof APP_ENV = {
     runtimeChecks: true,
+    logExceptions: false,
+    loggerUrl: 'http://localhost:3000'
   };
